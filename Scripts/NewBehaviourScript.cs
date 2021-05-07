896413720a3b1334ec3862e77799507a8042aa9d
songii2357@sogang.ac.kr <songeunii2357@gmail.com>
2021-05-07 21:26:51 +0900

    unitystudy

---
+++ Scripts/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float movementSpeed = 3.0f;
+    Vector3 movement = new Vector3();
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+        Rotate();
+    }
+}
